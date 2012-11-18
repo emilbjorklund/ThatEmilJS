9a38db898539b06da61d0fab2e258d407354a33b
Emil Björklund <emil.bjorklund@inuse.se>
2012-11-18 17:49:16 +0000

    Added new test.

--- test/namespace-test.js
+++ test/namespace-test.js
@@ -15,10 +15,16 @@ describe("The namespace method", function () {
 		app.namespace('nstest');
 		expect(app.nstest).toBeObject();
 	});
+	it("should use the path argument to create arbitrary levels of objects", function () {
+		app.namespace('nstest.foo.bar.baz');
+		expect(app.nstest.foo.bar.baz).toBeObject();
+	});
 	it("should not overwrite existing objects", function () {
 		app.nstest = { nested: {} };
 		var result = app.namespace("nstest.nested");
 		expect(app.nstest.nested).toBe(result);
 	});
-	
+	it("should throw an error when receiving a non-string path argument", function () {
+		expect(function () { app.namespace([]); }).toThrow('TypeError');
+	});
 });
